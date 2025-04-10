199a018b5f0d0c0a58f41903dbfbd7b53e2a789a
adoxa7 <159995968+adoxa7@users.noreply.github.com>
2025-04-10 20:26:42 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
-import asyncio
 from daily_journal_bot import flask_app, run
+import asyncio
 
-asyncio.create_task(run())  # запуск бота параллельно Flask
+asyncio.get_event_loop().create_task(run())  # корректный запуск фоновой задачи
+
 app = flask_app
