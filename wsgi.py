a2ead539df9a4edbbd851b0919f13c7c71711b71
adoxa7 <159995968+adoxa7@users.noreply.github.com>
2025-04-10 20:32:47 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from daily_journal_bot import flask_app, run
+from starlette.middleware.wsgi import WSGIMiddleware
 import asyncio
 
-asyncio.get_event_loop().create_task(run())  # корректный запуск фоновой задачи
-
-app = flask_app
+asyncio.get_event_loop().create_task(run())
+app = WSGIMiddleware(flask_app)
